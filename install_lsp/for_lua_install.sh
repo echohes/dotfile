027db3d8b208f186b7a5084f5acb75349d667203
Vadim Gaisin <hes88@mail.ru>
2022-10-25 23:32:17 +0300

    add PATH

--- install_lsp/for_lua_install.sh
+++ install_lsp/for_lua_install.sh
@@ -2,7 +2,6 @@
 set -o errexit
 set -o nounset
 
-
 echo "Install Lua-Language-Server"
 #LUA
 sudo apt -y install gcc g++ clang ninja-build
@@ -18,4 +17,6 @@ cd 3rd/luamake
 ./compile/install.sh
 cd ${HOME}/.config/lsp/lua-language-server/ 
 ./3rd/luamake/luamake rebuild
+
+echo 'export PATH="${HOME}/.config/lsp/lua-language-server/bin:${PATH}"' >> ~/.bashrc
 
